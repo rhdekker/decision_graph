48b5f928a37696ad7b211216da6fb2377ce51e17
djbpitt <djbpitt@pitt.edu>
2021-02-13 15:31:06 -0500

    Trying to execute a shared file

--- test.py
+++ test.py
@@ -4,3 +4,13 @@ now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
 # I can type in it
+# I can still type in it
+print('changes made after commit')
+print('david is typing')
+print('''dssd
+sd
+sd
+ss
+dsd
+
+sd''')
